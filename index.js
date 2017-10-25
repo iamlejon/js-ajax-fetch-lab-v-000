594032ab1abda536fa0890ba84850ad2241f862f
Dominique McCloud <niquelejon@gmail.com>
2017-10-25 01:37:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function showResults(json) {
 function forkRepo() {
   const repo = 'learn-co-curriculum/javascript-fetch-lab'
   //use fetch to fork it!
-  fetch('https://github.com/learn-co-curriculum/javascript-fetch-lab')
+  fetch('learn-co-curriculum/javascript-fetch-lab')
   .then(resp => resp.json())
   .then(json => console.log(json));
 }
